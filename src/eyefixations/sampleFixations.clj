ae127558d62abe106f16df40587f24498d4e1688
Juho Salmio <juho.salmio@gmail.com>
2013-10-12 17:42:52 +0300

    plot sample fixations

---
+++ src/eyefixations/sampleFixations.clj
@@ -0,0 +1,33 @@
+(ns eyefixations.sampleFixations
+ (:use eyefixations.core)
+ (:use clojure.contrib.math))
+
+(defn framesInRange [rangeMap frameRate]
+  (let [duration (- (:endTime rangeMap) (:startTime rangeMap))]
+    (round (/ duration (float (/ 1000 frameRate))))))
+
+(defn getRangesBetweenFixations [fixationData]
+  (map (fn[pair] {:startTime (:endTime (first pair))
+                  :endTime (:startTime (second pair))})
+       (partition 2 1 fixationData)))
+
+
+(defn sampleFixationFrames [fixationData frameRate & options]
+  "assumes that there is always fixation at startTime 0"
+  (let [frameCount (first options)
+        frames (flatten (map-indexed (fn [index frameRange]
+                                     (repeat (framesInRange frameRange frameRate)
+                                             {:x (:x frameRange)
+                                              :y (:y frameRange)
+                                              :fixation (even? index)}))
+              (interleave fixationData (getRangesBetweenFixations fixationData))))]
+      (if frameCount
+        (take frameCount frames)
+        frames)))
+
+(defn parseRawFixationData []
+  (let [columnMappings {"Position X" :x
+                        "Position Y" :y
+                        "Fixation Start [ms]" :startTime
+                        "Fixation End [ms]" :endTime}]
+    (parseData "resources/sample1_fixations.txt" columnMappings)))
--- src/eyefixations/sketcher.clj
+++ src/eyefixations/sketcher.clj
@@ -1,30 +1,48 @@
 (ns eyefixations.sketcher
   (:use quil.core
-        [quil.helpers.seqs :only [seq->stream range-incl]])
+        [quil.helpers.seqs :only [seq->stream]])
   (:require [eyefixations.velocityBasedIdentification :as simpleVelBasedIdentification])
-  (:use eyefixations.core))
+  (:use eyefixations.core)
+  (:use eyefixations.sampleFixations))
 
 (defn draw []
-  (let [frameData ((state :framedata))]
+  (let [data (state :data)
+        sampleFixationData ((:sampleData data))
+        veloFixationData ((:velocityData data))]
     (background 180)
-    (if (:fixation frameData)
-      (ellipse (:x frameData) (:y frameData) 20 20))))
+    (if (:fixation sampleFixationData)
+      (do
+        (fill 60 60 60)
+        (ellipse (:x sampleFixationData) (:y sampleFixationData) 30 30)))
 
+    (if (:fixation veloFixationData)
+      (do
+        (fill 255 255 255)
+        (ellipse (:x veloFixationData) (:y veloFixationData) 20 20)))
+    ))
 
 (defn setup []
   (smooth)
   (background 0)
-  (frame-rate 30)
-  (let [sampleSize 5000
-        velocityTreshold 20
-        data (simpleVelBasedIdentification/labeledPoints
-              (parseRawSampleData "resources/sample1.txt" sampleSize)
-              velocityTreshold )]
 
-  (set-state! :framedata (seq->stream data))))
+  (let [sampleSize 1000
+        velocityTreshold 10
+        frameRate 10
+        fixationFrames (sampleFixationFrames
+                            (parseRawFixationData)
+                            30
+                            sampleSize)
+        velocityFixationData (simpleVelBasedIdentification/labeledPoints
+                              (parseRawSampleData sampleSize)
+                               velocityTreshold )]
+  (frame-rate frameRate)
+  ;frame sequences are transformed to streams so when they are accessed from
+  ;the draw function they will allways return the value of the next frame
+  (set-state! :data {:sampleData (seq->stream fixationFrames)
+                     :velocityData (seq->stream velocityFixationData)})))
 
 (defsketch gen-art-24
-  :title "Animated Fluffy Clouds"
+  :title "Eyefixation viz"
   :setup setup
   :draw draw
   :size [1000 1000])
